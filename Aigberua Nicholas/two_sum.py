778bb8474bfdb4131c7cc57bb36eda9d87c6478a
nicholas <aigberuan6@gmail.com>
2024-12-04 13:36:58 +0100

    .

--- Aigberua Nicholas/two_sum.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def twosum(self, nums:list[int], target:int) -> list[int]:
-        hashmap = {}
-        for i in range (len(nums)):
-            dif = target - nums[i]
-            if dif in hashmap:
-                return (i, hashmap[dif[i]])
-            hashmap[dif[i]] = i
